916e639762fbf14ecf306a80da145b09056d9408
Arnaldo F. Costa <arnaldo.ferreira.costa@gmail.com>
2023-02-13 09:45:55 -0300

    Formulario em React

--- React/vite-react/src/App.tsx
+++ React/vite-react/src/App.tsx
@@ -1,45 +1,37 @@
 import {Header} from "./components/Header";
 import { Photo } from "./components/Photo";
-import { useState } from "react";
+import React, { useState } from "react";
 
 
-const App = () => {
-  const [n, setN] = useState(0);
-  const handleMinus = () => {
-    setN(n-1);
-  }
-  const handleMore = () => {
-    setN(n+1);
-  }
+
   
   
-  return (
-    <div>
-      <button onClick={handleMinus}>-</button>
-      <div>{n}</div>
-      <button onClick={handleMore}>+</button>
-    </div>
-  )
-}
-/*const App = ()=> {
-  const [nome, setName] = useState("Arnaldo");
+const App = ()=> {
+  const [nome, setName] = useState("");
   const handleClick= () => { 
     setName("Samanta");
-    
-};
+}
+  
+    const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  }  
+
 
 return (
   
   <div>
-    <Header title="Aqui fica o título"/>
-    Olá mundo!
-    <hr/>
-    <Photo url="http://www.google.com.br/google.jpg" legend="Google"/>
+  Nome: <input type= "text" value={nome} onChange= {handleInput}/>
+    
     The name is:<br/>{nome}
-    <button onClick={handleClick}>Clique teste</button>
+    <button onClick={handleClick}>Clique teste</button> <hr/>
+    <Photo url="http://www.google.com.br/google.jpg" legend="Google"/>
+    
   </div>
+  
 )
-};*/
+};
+
+
 
 
 
