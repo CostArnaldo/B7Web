e24980c991c198bef32c019b2aa28d66acbf531d
Arnaldo F. Costa <arnaldo.ferreira.costa@gmail.com>
2023-03-28 11:25:51 -0300

    movies react 28-03

--- React/módulo5/cicloDeVida-requisição-hooks/src/App.tsx
+++ React/módulo5/cicloDeVida-requisição-hooks/src/App.tsx
@@ -1,11 +1,26 @@
-import { ChangeEvent, useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React, { ChangeEvent, useEffect, useState } from 'react'
 import './App.css'
+import {Movie} from "./types/Movie"
+
+
 
 function App() {
   const [name, setName] = useState("");
   const [lastN, setLastN] = useState("");
   const [fullN, setFullN] = useState("");
+  
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const loadmovies = () => {
+    fetch("https://api.b7web.com.br/cinema/")
+    .then((response) => {
+    
+        return response.json();
+    })
+    .then ((json) => {
+        setMovies(json);
+    });
+}
+
 
   const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {      
 
@@ -24,7 +39,21 @@ function App() {
     <div>
       <input type= "text" placeholder='Seu NOME' value= {name} onChange={handleNameChange}/>
       <input type= "text" placeholder='Seu SOBRENOME' value= {lastN} onChange={handleLastNChange}/>
-      <p>Nome Completo: {fullN}</p>
+      <p>Nome Completo: {fullN}</p> 
+      <hr/>
+      <div>
+        <button className="botao" onClick={loadmovies}>Carregar Filmes</button>
+
+        Total de Filmes: {movies.length}
+        <div  className='listMovies'>
+          {movies.map((item, index) => (
+              <div key={index}>
+                <img src={item.avatar} className="imagem"/>
+                {item.titulo}
+              </div>
+          ))}
+        </div>
+    </div>
     </div>
   )
 }
